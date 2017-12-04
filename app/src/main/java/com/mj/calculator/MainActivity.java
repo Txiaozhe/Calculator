00cf14e16e27845b4361838264f23ed75da4a874
TangXiaoji <txiaozhe@gmail.com>
2017-12-04 08:45:00 +0800

    功能基本完成

--- app/src/main/java/com/mj/calculator/MainActivity.java
+++ app/src/main/java/com/mj/calculator/MainActivity.java
@@ -2,11 +2,9 @@ package com.mj.calculator;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Path;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 enum Operate {
     NONE, SQUARE, DIVIDE, MULTIPLY, DECREASE, INCREASE
@@ -23,7 +21,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private String show = ""; // 用于保存需要显示的文字
     private String num1 = "";
     private String num2 = "";
-    private String result = "0";
 
     private TextView tv_num;
 
@@ -60,7 +57,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     // 计算平方根
     private String square(String n) {
         Double r = Double.parseDouble(n);
-        if(r < 0) {
+        if (r < 0) {
             return "只能对大于0的数开平方！";
         }
         return String.valueOf(Math.sqrt(r));
@@ -83,109 +80,208 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     // 计算除法
     private String divide(String n1, String n2) {
-        String r = "";
-        try {
-            r = String.valueOf(Double.parseDouble(n1) / Double.parseDouble(n2));
-        } catch (Exception e) {
-            r = "请检查输入！";
+        if(n2.equals("0")) {
+            return "请检查输入";
         }
-        return (r);
+        return String.valueOf(Double.parseDouble(n1) / Double.parseDouble(n2));
+    }
+
+    // 计算百分比
+    private String percent(String n) {
+        if(!n.equals("0") && !n.equals("")) {
+            return String.valueOf(Double.parseDouble(n) / 100);
+        }
+
+        return n;
+    }
+
+    // 设置操作符类型
+    private void setOperate(Operate o) {
+        op = o;
+    }
+
+    // 识别操作符类型是否是+ - * /
+    private boolean isOperator() {
+        return op != Operate.NONE;
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.bt_ac : {
+            case R.id.bt_ac: {
                 tv_num.setText("");
                 show = "";
                 num1 = "";
                 num2 = "";
+                this.setOperate(Operate.NONE);
                 break;
             }
-            case R.id.bt_square : {
-                if(op == Operate.NONE) {
+            case R.id.bt_square: {
+                if (op == Operate.NONE) {
+                    this.setOperate(Operate.SQUARE);
                     show = this.square(show);
                     tv_num.setText(show);
-                    show = "";
                 }
 
                 break;
             }
-            case R.id.bt_percent : {
+            case R.id.bt_percent: {
+                show = this.percent(show);
+                tv_num.setText(show);
+                break;
+            }
+            case R.id.bt_divide: {
+                this.setOperate(Operate.DIVIDE);
+                num1 = show;
+                break;
+            }
+            case R.id.bt_multiply: {
+                this.setOperate(Operate.MULTIPLY);
+                num1 = show;
+                break;
+            }
+            case R.id.bt_dec: {
+                this.setOperate(Operate.DECREASE);
+                num1 = show;
+                break;
+            }
+            case R.id.bt_inc: {
+                this.setOperate(Operate.INCREASE);
+                num1 = show;
+                break;
+            }
+            case R.id.bt_equal: {
+                switch (op) {
+                    case INCREASE: {
+                        show = this.increase(num1, show);
+                        tv_num.setText(show);
+                        break;
+                    }
+                    case DECREASE: {
+                        show = this.decrease(num1, show);
+                        tv_num.setText(show);
+                        break;
+                    }
+                    case MULTIPLY: {
+                        show = this.multiply(num1, show);
+                        tv_num.setText(show);
+                        break;
+                    }
+                    case DIVIDE: {
+                        show = this.divide(num1, show);
+                        tv_num.setText(show);
+                        break;
+                    }
+                    case NONE: {
+
+                    }
+                }
+                break;
+            }
+            case R.id.bt_dot: {
+                show += ".";
+                tv_num.setText(show);
+                break;
+            }
+            case R.id.bt_0: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "0";
+                    tv_num.setText(show);
+                } else {
+                    show += "0";
+                    tv_num.setText(show);
+                }
+                break;
+            }
+            case R.id.bt_1: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "1";
+                    tv_num.setText(show);
+                } else {
+                    show += "1";
+                    tv_num.setText(show);
+                }
 
                 break;
             }
-            case R.id.bt_divide : {
-
+            case R.id.bt_2: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "2";
+                    tv_num.setText(show);
+                } else {
+                    show += "2";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_multiply : {
-
+            case R.id.bt_3: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "3";
+                    tv_num.setText(show);
+                } else {
+                    show += "3";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_dec : {
-
+            case R.id.bt_4: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "4";
+                    tv_num.setText(show);
+                } else {
+                    show += "4";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_inc : {
-
+            case R.id.bt_5: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "5";
+                    tv_num.setText(show);
+                } else {
+                    show += "5";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_equal : {
-
+            case R.id.bt_6: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "6";
+                    tv_num.setText(show);
+                } else {
+                    show += "6";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_dot : {
-
+            case R.id.bt_7: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "7";
+                    tv_num.setText(show);
+                } else {
+                    show += "7";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_0 : {
-
+            case R.id.bt_8: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "8";
+                    tv_num.setText(show);
+                } else {
+                    show += "8";
+                    tv_num.setText(show);
+                }
                 break;
             }
-            case R.id.bt_1 : {
-                show += "1";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_2 : {
-                show += "2";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_3 : {
-                show += "3";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_4 : {
-                show += "4";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_5 : {
-                show += "5";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_6 : {
-                show += "6";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_7 : {
-                show += "7";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_8 : {
-                show += "8";
-                tv_num.setText(show);
-                break;
-            }
-            case R.id.bt_9 : {
-                show += "9";
-                tv_num.setText(show);
+            case R.id.bt_9: {
+                if (!show.equals("") && this.isOperator()) {
+                    show = "9";
+                    tv_num.setText(show);
+                } else {
+                    show += "9";
+                    tv_num.setText(show);
+                }
                 break;
             }
         }
--- app/src/main/java/com/mj/calculator/RateActivity.java
+++ app/src/main/java/com/mj/calculator/RateActivity.java
@@ -5,13 +5,34 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by txiaozhe on 02/12/2017.
  */
 
 public class RateActivity extends Activity {
-    private Button bt_back;
+
+    private double rate = 6.6137;
+    private String[] coins = new String[]{"人民币", "美元"};
+
+    private TextView tv_coin1;
+    private TextView tv_coin2;
+    private TextView tv_result;
+
+    private double money = 0;
+    private EditText et_money;
+
+    private double $toRmb(double $) {
+        return $ / rate;
+    }
+
+    private double rmbTo$(double r) {
+        return r * rate;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +47,44 @@ public class RateActivity extends Activity {
             @Override
             public void onClick(View view) {
                 RateActivity.this.finish();
+            }
+        });
+
+        tv_coin1 = findViewById(R.id.tv_coin1);
+        tv_coin2 = findViewById(R.id.tv_coin2);
+        tv_coin1.setText(coins[0]);
+        tv_coin2.setText(coins[1]);
+        findViewById(R.id.bt_exchange).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String p = coins[0];
+                coins[0] = coins[1];
+                coins[1] = p;
+                tv_coin1.setText(coins[0]);
+                tv_coin2.setText(coins[1]);
+            }
+        });
+
+        et_money = findViewById(R.id.et_money);
+        tv_result = findViewById(R.id.tv_result);
+        findViewById(R.id.bt_calculate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String m = et_money.getText().toString();
+                if(!m.equals("")) {
+                    money = Double.parseDouble(m);
+                    if(coins[0].equals("人民币")) {
+                        tv_result.setText(String.valueOf(money / rate));
+                    } else {
+                        tv_result.setText(String.valueOf(money * rate));
+                    }
+                } else {
+                    if(coins[0].equals("人民币")) {
+                        tv_result.setText(String.valueOf(1 / rate));
+                    } else {
+                        tv_result.setText(String.valueOf(1 * rate));
+                    }
+                }
             }
         });
     }
